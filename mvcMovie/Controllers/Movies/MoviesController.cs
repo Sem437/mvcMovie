968d7fd02cf08df8d358a51645dcd3611af4558c
sem <semdronten@gmail.com>
2024-06-14 09:08:43 +0200

    Filterbar in /Movies toegevoegd

--- mvcMovie/Controllers/Movies/MoviesController.cs
+++ mvcMovie/Controllers/Movies/MoviesController.cs
@@ -23,7 +23,8 @@ namespace mvcMovie.Controllers.Movies
 
         // GET: Movies
         [Authorize(Roles = "AdminOnly")]
-        public async Task<IActionResult> Index(string MovieTitle, int? minMovieTime, int? maxMovieTime)
+        public async Task<IActionResult> Index(string MovieTitle, int? minMovieTime, int? maxMovieTime, string timeOrder,
+            int? releaseYear, string releaseOrder, string genreFilter)
         {
             if (_context.Movie == null)
             {
@@ -36,6 +37,84 @@ namespace mvcMovie.Controllers.Movies
             if (!String.IsNullOrEmpty(MovieTitle))
             {
                 movies = movies.Where(s => s.Title!.Contains(MovieTitle));
+                ViewData["titleMovie"] = MovieTitle;
+            }
+
+            if (minMovieTime.HasValue)
+            {
+
+                if (minMovieTime.HasValue && maxMovieTime.HasValue)
+                {
+                    movies = movies.Where(s => s.MovieTime >= minMovieTime && s.MovieTime <= maxMovieTime);
+                    ViewData["minMovieTime"] = minMovieTime;
+                    ViewData["maxMovieTime"] = maxMovieTime;
+                }
+                else if (minMovieTime.HasValue)
+                {
+                    movies = movies.Where(s => s.MovieTime >= minMovieTime);
+                    ViewData["minMovieTime"] = minMovieTime;
+                }
+                else if (maxMovieTime < minMovieTime)
+                {
+                    return View(await movies.ToListAsync());
+                }
+
+            }
+
+            if (!String.IsNullOrEmpty(timeOrder))
+            {
+                switch (timeOrder)
+                {
+                    case "Low2High":
+                        movies = movies.OrderBy(m => m.MovieTime);
+                        break;
+                    case "High2Low":
+                        movies = movies.OrderByDescending(m => m.MovieTime);
+                        break;
+                }
+                ViewData["timeOrder"] = timeOrder;
+            }
+
+            if (releaseYear.HasValue)
+            {
+                var releaseYearString = releaseYear.Value.ToString();
+                movies = movies.Where(s => s.ReleaseData.ToString().Contains(releaseYearString)); //Contains = %releaseYear%
+                ViewData["releaseYear"] = releaseYear;
+            }
+
+            if (!String.IsNullOrEmpty(releaseOrder))
+            {
+                switch (releaseOrder)
+                {
+                    case "new2old":
+                        movies = movies.OrderByDescending(m => m.ReleaseData);
+                        break;
+                    case "old2new":
+                        movies = movies.OrderBy(m => m.ReleaseData);
+                        break;
+                }
+                ViewData["releaseOrder"] = releaseOrder;
+            }
+
+            if (!String.IsNullOrEmpty(genreFilter))
+            {
+                var genreFilterString = genreFilter.ToString();
+
+                switch (genreFilter)
+                {
+                    case "Action":
+                        movies = movies.Where(s => s.Genre.ToString().Contains(genreFilterString));
+                        break;
+                    case "Crime":
+                        movies = movies.Where(s => s.Genre.ToString().Contains(genreFilterString));
+                        break;
+                    case "Comedy":
+                        movies = movies.Where(s => s.Genre.ToString().Contains(genreFilterString));
+                        break;
+                    case "Science Fiction":
+                        movies = movies.Where(s => s.Genre.ToString().Contains(genreFilterString));
+                        break;
+                }
             }
 
             return View(await movies.ToListAsync());
